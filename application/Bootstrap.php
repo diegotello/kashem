c47e32f7d9afec40475d6e24cd6be5e44ff9add3
Diego Tello <dietello1@gmail.com>
2013-11-02 20:39:58 -0600

    Init acl and routing

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -1,8 +1,30 @@
 <?php
 
-class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
-{
+class Bootstrap extends Zend_Application_Bootstrap_Bootstrap {
 
+    protected function _initRouting() {
+        $config = new Zend_Config_Ini(
+                APPLICATION_PATH . "/configs/routes.ini", APPLICATION_ENV
+        );
+        $router = new Zend_Controller_Router_Rewrite();
+        $router->addConfig($config, "routes");
+        Zend_Controller_Front::getInstance()->setRouter($router);
+    }
+
+    protected function _initLoadAclConfig() {
+        $config = new Zend_Config_Ini(APPLICATION_PATH . '/configs/acl.ini', APPLICATION_ENV);
+        Zend_Registry::set('acl', $config);
+    }
+
+    protected function _initAclControllerPlugin() {
+        $this->bootstrap('frontcontroller');
+        $this->bootstrap('loadAclConfig');
+
+        $front = Zend_Controller_Front::getInstance();
+        $acl = new Kashem_Acl();
+        $aclPlugin = new Kashem_AuthPlugin($acl);
+        $front->registerPlugin($aclPlugin);
+    }
 
 }
 

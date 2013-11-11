8cb6d7a1f75b1215cd90b7fb85de097c85baaab2
diegotello <dietello1@gmail.com>
2013-11-11 13:40:57 -0600

    include fetchAll, find and findAsArray on ClienteMapper

--- application/models/ClienteMapper.php
+++ application/models/ClienteMapper.php
@@ -52,5 +52,87 @@ class Kashem_Model_ClienteMapper {
         }
     }
 
+    public function fetchAll() {
+        $resultSet = $this->getDbTable()->fetchAll();
+        $entries = array();
+        $pm = new Kashem_Model_PaisMapper();
+        $dm = new Kashem_Model_DepartamentoMapper();
+        $mm = new Kashem_Model_MunicipioMapper();
+        foreach ($resultSet as $row) {
+            $pais = new Kashem_Model_Pais();
+            $departamento = new Kashem_Model_Departamento();
+            $municipio = new Kashem_Model_Municipio();
+            $pm->find($row->pais_id, $pais);
+            $dm->find($row->departamento_id, $departamento);
+            $mm->find($row->municipio_id, $municipio);
+            $entry = new Kashem_Model_Cliente();
+            $entry->setId($row->id)
+                    ->setContactoEmergencia($row->contacto_emergencia)
+                    ->setCorreoElectronico($row->correo_electronico)
+                    ->setDepartamento($departamento)
+                    ->setDireccion($row->direccion)
+                    ->setDpi($row->dpi)
+                    ->setFechaNacimiento($row->fecha_nacimiento)
+                    ->setGenero($row->genero)
+                    ->setMunicipio($municipio)
+                    ->setObservacionGeneral($row->observacion_general)
+                    ->setObservacionMedica($row->observacion_medica)
+                    ->setPais($pais)
+                    ->setPrimerApellido($row->primer_apellido)
+                    ->setPrimerNombre($row->primer_nombre)
+                    ->setSegundoApellido($row->segundo_apellido)
+                    ->setSegundoNombre($row->segundo_nombre)
+                    ->setTelefonoEmergencia($row->telefono_emergencia)
+                    ->setTelefono($row->telefono)
+                    ->setUsuarioFacebook($row->usuario_facebook);
+            $entries[] = $entry;
+        }
+        return $entries;
+    }
+
+    public function find($id, Kashem_Model_Cliente $cliente) {
+        $result = $this->getDbTable()->find($id);
+        if (0 == count($result)) {
+            return;
+        }
+        $row = $result->current();
+        $pm = new Kashem_Model_PaisMapper();
+        $dm = new Kashem_Model_DepartamentoMapper();
+        $mm = new Kashem_Model_MunicipioMapper();
+        $pais = new Kashem_Model_Pais();
+        $departamento = new Kashem_Model_Departamento();
+        $municipio = new Kashem_Model_Municipio();
+        $pm->find($row->pais_id, $pais);
+        $dm->find($row->departamento_id, $departamento);
+        $mm->find($row->municipio_id, $municipio);
+        $cliente->setId($row->id)
+                ->setContactoEmergencia($row->contacto_emergencia)
+                ->setCorreoElectronico($row->correo_electronico)
+                ->setDepartamento($departamento)
+                ->setDireccion($row->direccion)
+                ->setDpi($row->dpi)
+                ->setFechaNacimiento($row->fecha_nacimiento)
+                ->setGenero($row->genero)
+                ->setMunicipio($municipio)
+                ->setObservacionGeneral($row->observacion_general)
+                ->setObservacionMedica($row->observacion_medica)
+                ->setPais($pais)
+                ->setPrimerApellido($row->primer_apellido)
+                ->setPrimerNombre($row->primer_nombre)
+                ->setSegundoApellido($row->segundo_apellido)
+                ->setSegundoNombre($row->segundo_nombre)
+                ->setTelefonoEmergencia($row->telefono_emergencia)
+                ->setTelefono($row->telefono)
+                ->setUsuarioFacebook($row->usuario_facebook);
+    }
+
+    public function findAsArray($id) {
+        $result = $this->getDbTable()->find($id);
+        if (0 == count($result)) {
+            return;
+        }
+        return $result->current();
+    }
+
 }
 

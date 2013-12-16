3b1e6c69c100cdb5e7a6912a29f968a5d9620c30
Diego Tello <dietello1@gmail.com>
2013-12-16 11:53:50 -0600

    get date string function

--- public/js/reporte/comun.js
+++ public/js/reporte/comun.js
@@ -62,3 +62,30 @@ pageConfig = {
         refresh: "Recargar la tabla"
     }
 };
+
+function getDateString() {
+    var today = new Date(),
+            dd = today.getDate(),
+            mm = today.getMonth() + 1,
+            yyyy = today.getFullYear(),
+            h = today.getHours(),
+            min = today.getMinutes(),
+            sec = today.getSeconds();
+    if (dd < 10) {
+        dd = '0' + dd;
+    }
+    if (mm < 10) {
+        mm = '0' + mm;
+    }
+    if (h < 10) {
+        h = '0' + h;
+    }
+    if (min < 10) {
+        min = '0' + min;
+    }
+    if (sec < 10) {
+        sec = '0' + sec;
+    }
+    today = dd + '_' + mm + '_' + yyyy + '_' + h + '_' + min + '_' + sec;
+    return today;
+}
